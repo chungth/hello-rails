eb209ecc77c028b82351e237a6e17328f7fa7598
Chung Tran <huychungtran@gmail.com>
2014-11-06 11:56:35 +0900

    solved chapter 6 exercise

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,10 @@
 class User < ActiveRecord::Base
   # like Laravel Eloquent event
-  before_save{ self.email = email.downcase}
+  before_save{ email.downcase!}
   #validates is method validates(:name, presence:true)
   validates :name, presence: true, length: {maximum: 50}
-  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  #old: VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
   validates :email,presence: true, length: {maximum: 255}, 
                   format:{with: VALID_EMAIL_REGEX},
                   uniqueness: { case_sensitive: false}
